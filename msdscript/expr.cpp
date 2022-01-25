8976add01b3c93efef2b97461ea955c923d3c7c4
jinGu <gj1308838@gmail.com>
2022-01-25 01:14:50 -0700

    add some change

--- msdscript/expr.cpp
+++ msdscript/expr.cpp
@@ -9,6 +9,40 @@
 #include <sstream>
 using namespace std;
 
+string s1 = "--help";
+string s2 = "--test";
+int use_arguments(int argc, char **argv)
+{
+    bool isTest = false;
+    if(argc>1) {
+        //first identify the length of command, if the length is 1 no command input
+        for (int i = 1; i < argc; i++) {
+            //help command input
+            if (argv[i] == s1) {
+                cout << "loading the help function, please wait...." << endl;
+
+                exit(0);
+            }
+                //test command input
+            else if (argv[i] == s2) {
+                if (!isTest) {
+                    Catch::Session().run(1, argv);
+                    isTest = true;
+                } else {
+                    cerr << "Has tested before." << endl;
+                    exit(1);
+                }
+            }
+                //if test 2 times more or input other command not valid
+            else {
+                cerr << "wrong input, please type in other command\n";
+                exit(1);
+            }
+        }
+
+    }
+    return 0;
+}
 //override pure virtual method of interface
 //Class Num--------------------------------------------------------------------------
 Num::Num(int val){
--- msdscript/main.hpp
+++ msdscript/main.hpp
@@ -6,7 +6,6 @@
 #define main_hpp
 
 #include <stdio.h>
-#include "expr.cpp"
 //just need to include this cpp file and change the make file to compile just the main file cause we have included the
 //expr so fo not need to compile it again
 int use_arguments(int argc, char **argv);
