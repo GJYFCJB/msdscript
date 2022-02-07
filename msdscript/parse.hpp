a2367fab2e98e53a26665f8473d51a87c281407b
jinGu <gj1308838@gmail.com>
2022-02-07 13:49:40 -0700

    add parse class

---
+++ msdscript/parse.hpp
@@ -0,0 +1,16 @@
+//
+// Created by 谷金 on 2/6/22.
+//
+
+#ifndef parse_hpp
+#define parse_hpp
+
+#include <sstream>
+#include <iostream>
+
+//    expr *parse_num(std::istream &in);
+
+
+
+
+#endif //CS6015PROJECT_PARSE_HPP
